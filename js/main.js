f4bbdc104d4566f44fa97310cf4579fb8229a6a5
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-18 11:56:42 +0300

    fn printToHtml + div#output

--- js/main.js
+++ js/main.js
@@ -4,3 +4,8 @@ console.log('main.js file was loaded');
 function sum(a, b, c) {
   return a + b + c;
 }
+
+function printToHtml(whatToPrint) {
+  const dest = document.getElementById('output');
+  dest.append(whatToPrint);
+}
